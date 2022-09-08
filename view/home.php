0d096dc775794c5e060956d31c97c0d5925b83e4
Kevin Faccin <devbunker2022@gmail.com>
2022-09-08 18:29:17 -0300

    first commit

---
+++ view/home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<title>Plenus</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/menu.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/home.css">
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.css">
+
+	<script src="assets/bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+
+	<?php include 'menu.php'; ?>
+
+	<div class="row" id="general-content">
+		<div class="container-fluid" id="img-fundo-top">
+			<div id="bottom-content">
+				<div class="row">
+					<div class="col-lg-4 col-md-4 col-sm-1"></div>
+					<div class="col-lg-7 col-md-7 col-sm-12">
+						<div class="card" id="card">
+							<div class="card-body">
+								<h5 class="card-title">PLENUS GESTÃO DE TALENTOS</h5>
+								<p class="card-text">Constituída em 17 de maio de 2020, com o propósito de
+									prestar serviço diferenciado, tem como foco atender as empresas com agilidade,
+									desenvolvendo e qualificando o mercado de trabalho, através da contratação de
+								profissionas efetivos, treinamento e consultoria em Recursos Humanos.</p>
+
+								<a href="#" class="card text-end">Card link</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-2 col-md-2 col-sm-2"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+
+</body>
+</html>
